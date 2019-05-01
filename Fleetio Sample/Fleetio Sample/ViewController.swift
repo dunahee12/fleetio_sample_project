538ab37ec30decd31110d5186bceee456884bc2f
Jake Dunahee <dunahee12@gmail.com>
2019-05-01 15:39:04 -0500

    Inital project creation

---
+++ Fleetio Sample/Fleetio Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fleetio Sample
+//
+//  Created by Jake Dunahee on 5/1/19.
+//  Copyright © 2019 Jake Dunahee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
